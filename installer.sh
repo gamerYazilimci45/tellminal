3ca1f0e494f49a9eeec058f816d0c5db33204493
gamerYazilimci67 <abdulbakisari54@gmail.com>
2025-01-01 20:42:54 +0300

    update beta

--- installer.sh
+++ installer.sh
@@ -6,21 +6,20 @@ TELLMINAL="https://github.com/gamerYazilimci45/tellminal/raw/main/tellminal.py"
 SERVER_PY="https://github.com/gamerYazilimci45/tellminal/raw/main/server.py"
 CLIENT_PY="https://github.com/gamerYazilimci45/tellminal/raw/main/client.py"
 
-DEST_DIR="/usr/bin"
-DEST_DIR2="/usr/local/bin"
+DEST_DIR="/usr/local/bin"
 TELLMINAL_NAME="tellminal"
 
 echo "Files are downloading and moving..."
 for PY_FILE in "$SERVER_PY" "$CLIENT_PY"; do
     BASENAME=$(basename "$PY_FILE")
     echo "Downloading: $BASENAME"
-    sudo wget -q -O "$DEST_DIR2/$BASENAME" "$PY_FILE"
-    sudo chmod +x "$DEST_DIR2/$BASENAME"
+    sudo wget -q -O "$DEST_DIR/$BASENAME" "$PY_FILE"
+    sudo chmod +x "$DEST_DIR/$BASENAME"
 done
 
 BASENAME=$(basename "$TELLMINAL")
 echo "Downloading: $BASENAME"
 sudo wget -q -O "$DEST_DIR/$TELLMINAL_NAME" "$TELLMINAL"
-sudo chmod +x "$DEST_DIR/$TELLMINAL_NAMEs"
+sudo chmod +x "$DEST_DIR/$TELLMINAL_NAME"
 
 echo "[+] tellminal is successfully installed!"
